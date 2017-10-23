e1094ab4399473f7ca2ae81d7ff55d8ce4862413
Tue Thomas Ha Minh Son <thom875f@edu.eal.dk>
2017-10-23 10:29:10 +0200

    What up bois, first commit

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class Amulet
+    {
+        string ItemID;
+        string Design;
+        enum Level { low, medium, high}
+        Level Quality;
+
+        public Amulet(string ItemID)
+        {
+
+        }
+        public Amulet(string ItemID, string Design)
+        {
+
+        }
+        public Amulet(string ItemID, string Design, string Quality)
+        {
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
---
+++ Morgengry/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class Book
+    {
+        string ItemID;
+        string Titel;
+        double price;
+       
+        public Book(string ItemID)
+        {
+        
+        }
+        public Book(string ItemID, string Titel)
+        {
+
+        }
+        public Book(string ItemID, string Titel, double price)
+        {
+        }
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
